e2b2d34ec8fb404eb1c971b5b6ac815a9a8cac56
Erik Christianson <internet.erik@gmail.com>
2016-05-11 14:56:12 -0500

    starting to add character interaction with objects

--- game.js
+++ game.js
@@ -12,8 +12,29 @@
     render();
   }
   //register a function to perform on spacebar
+  // This should see if there is an object to interact with within range of the
+  // direction the character is facing.
+  // if character is close to two things he will interact with the closer of them
+  // if there are two equally close, he will interact with the left one
+  // if facing a diagonal, objects on both sides of diagonal will be considered
   subscribeToKeyPress('32', function() {
-    console.log("act");
+    //arrays to store indexes to objects that can be interacted with
+    var stationaryNdxs = [];
+    var mobileNdx      = [];
+    console.log("interact");
+    //check if there is an object nearby
+    //this object will be a mobile or stationary object
+    for(let i = mobiles.length; i >= 0; i--)
+      if(isInRange())
+        mobileNdx.push(i);
+    for(let i = stationary.length; i >= 0; i--)
+      if(isInRange())
+        mobileNdx.push(i);
+
+    function isInRange(obj, character, facing) {
+      return true;
+    }
+    
   });
   //start the game loop
   var interval = setInterval(gameLoop.bind(this), 10);  
